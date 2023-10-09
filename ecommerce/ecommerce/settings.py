51efc559146e9027a508021d3e3eb354b6a7154a
Le-morosophe <88990197+Le-morosophe@users.noreply.github.com>
2023-10-09 16:57:23 -0500

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
-    'ecommerce.shop'
+    'ecommerce_ecommerce_shop'
 ]
 
 SITE_ID = 1
