89dae72e51bd13189c690f1a360f8f9f97672f67
Le-morosophe <88990197+Le-morosophe@users.noreply.github.com>
2023-10-10 08:37:59 -0500

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
-    'shop'
+    'ecommerce.shop'
 ]
 
 SITE_ID = 1
@@ -147,7 +147,7 @@ TEMPLATES = [
 
 ]
 
-WSGI_APPLICATION = 'ecommerce.wsgi.application'
+WSGI_APPLICATION = 'ecommerce.ecommerce.wsgi.application'
 
 
 # Database
